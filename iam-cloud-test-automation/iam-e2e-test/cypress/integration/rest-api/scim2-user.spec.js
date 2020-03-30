ba6c512c33ef70d7019df697cb7c9308852d4ddf
Sahan Ruwanga <sahanruwanga.15@cse.mrt.ac.lk>
2020-03-30 14:24:21 +0530

    Update for test case

--- iam-cloud-test-automation/iam-e2e-test/cypress/integration/rest-api/scim2-user.spec.js
+++ iam-cloud-test-automation/iam-e2e-test/cypress/integration/rest-api/scim2-user.spec.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+describe('Test cases for SAML SSO Scenarios', function () {
+ before(function () {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+
+    cy.visit('http://localhost:3000/app');
+
+ })
+
+  it('check whether user able to login to sample service provider through WSO2 IDP', function () {
+
+    cy.login();
+  })
+
+})
